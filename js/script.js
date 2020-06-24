3bc1b90ce8fc1ee786dbefe1ae99df928fcd5b16
Sangya2001 <sangyasherpa2058@gmail.com>
2020-06-24 12:02:15 +0545

    Product Design and Responsive is done.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-$('.carousel').carousel({
-    interval: 4000 // carousel changes in every 4sec
-  })
+// setting the carousel time
+$(".carousel").carousel({
+  interval: 4000, // carousel changes in every 4sec
+});
+
+// check if the product is hearted or not
+var isHearted = [false, false, false, false];
+
+const changeColor = (i) => {
+  if (!isHearted[i]) {
+    isHearted[i] = true;
+    document.getElementById(`heart${i}`).style.color = "#ffcb2e";
+  } else {
+    isHearted[i] = false;
+    document.getElementById(`heart${i}`).style.color = "#4a4a4a";
+  }
+};
